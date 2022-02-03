609a13a1dcf1b52f9d232b758e687c222462672f
Idioot <99066617@mydavinci.nl>
2022-02-03 11:39:24 +0100

    'test'

--- lightswitch/script.js
+++ lightswitch/script.js
@@ -1,10 +1,27 @@
-var btn = document.createElement('button');
-btn.style.margin = '10px';
-btn.innerHTML = '...';
-document.body.appendChild(btn);
+// schijf hier tussen je code'
+const status = ["Het licht is uit.", "Het licht is aan."];
+var counter = 0
+button.value = status[1]
 
-// schijf hier tussen je code
+function myFunction(){
+    if (counter > 1) {counter = 0}
+    button.value = status[counter]
+    
+    if (counter == 0) {
+        document.getElementById("kleur").classList.toggle('geel');
+        document.getElementById("kleur").classList.toggle('zwart');
+                console.log(status[0])
+        }
+            
 
+    else    {
+        document.getElementById("kleur").classList.toggle('geel');
+        document.getElementById("kleur").classList.toggle('zwart');
+            console.log(status[1])
+        }
+    
 
+    counter += 1    
 
+};   
 // schijf hier tussen je code
